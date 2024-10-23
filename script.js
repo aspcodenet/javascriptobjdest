76cdcb189340fc90f44a67627af99855c6c19a3e
stefan <stefan.holmberg@systementor.se>
2024-10-23 14:14:44 +0200

    a

--- script.js
+++ script.js
@@ -55,6 +55,31 @@ console.log("Ålder " + age)
 console.log("Namn  " + namn)
 
 
+const movies = [
+
+    { title: "The Fast and the Furious", year: 2001, rating: 4.2  },
+    
+    { title: "Fast & Furious 6", year: 2013, rating: 5.0 },
+    
+    { title: "Fast & Furious 7", year: 2015, rating: 3 },
+    
+    { title: "Fast & Furious 9", year: 2021, rating: 5 },
+    
+    ];
+
+// LINQ
+
+    // ta fram alla filmer som har minst 4.5 i betyg
+    const braMovies = movies.filter(m=>m.rating > 4.5)
+    console.log(braMovies)
+
+    // ta fram title (ska heta namn) och age (2024-year) för  alla filmer som har minst 4 i betyg
+    const braMovies2 = movies.filter(m=>m.rating > 4).map(m=>{   return { namn:m.title, age:2024-m.year }      })
+    console.log(braMovies2)
+
+
+
+
 
 
 
